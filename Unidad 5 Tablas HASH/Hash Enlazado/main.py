c97e59267dec6ea4a3af2b68ddfe4897b6dd74ef
Herman Gabriel Soria <103910909+HermanGS@users.noreply.github.com>
2024-10-14 19:19:39 -0300

    Add files via upload

--- Unidad 5 Tablas HASH/Hash Enlazado/main.py
+++ Unidad 5 Tablas HASH/Hash Enlazado/main.py
@@ -2,33 +2,87 @@ import numpy as np
 
 import random
 
-class objetoT:
-    __libre: bool
+class Nodo:
+    __dato : None
+    __siguiente : None
 
-    def __init__(self):
-        self.__dato = None
-        self.__libre = True 
-
-    def setDato(self,dato):
+    def __init__(self,dato):
         self.__dato = dato
+        self.__siguiente = None
 
-    def getDato(self):
+    def retornaSig(self):
+        return self.__siguiente
+    
+    def CambiarSig(self,x):
+        self.__siguiente = x
+        
+    def retornaDato(self):
         return self.__dato
     
+    def CambiarDato(self,x):
+        self.__dato = x
 
-    def getLibre(self):
-        return self.__libre
-    
 
-    def setOcupado(self):
-        self.__libre = False
 
-    def setLibre(self):
-        self.__libre = True
+class Pila:
+    #__items : np.ndarray No se NECESITA arreglo para esta Estructura, EL TOPE funciona como CABEZA de la lista PILA
+    __tope : None
+    __cant : int # La cantidad acá se usa de manera diferente, ya no es el tope de los ESPACIOS DISPONIBLES sino que es un CONTADOR me parece
+    #Contador de la cantida de NODOS que existen
 
-    
-    def __str__(self):
-        return f'{self.__dato}'
+    def __init__(self,tope = None, cant = 0) -> None:
+        self.__tope = tope
+        self.__cant = cant
+        #self.__items = np.empty
+
+    def vacia(self):
+        return (self.__cant == 0)
+
+    def insertar(self,x):
+        NuevoNodo = Nodo(x)
+        #NuevoNodo.CambiarDato(x)
+        NuevoNodo.CambiarSig(self.__tope)
+        self.__tope = NuevoNodo
+        self.__cant += 1
+        print(f"cantidad de elementos en pila : {self.__cant}")
+        return NuevoNodo.retornaDato()
+
+    def suprimir(self):
+        if self.vacia():
+            print("No se puede suprimir, esta vacia")
+        else:
+            aux = self.__tope
+            x = aux.retornaDato()
+            self.__tope = self.__tope.retornaSig()
+            self.__cant -= 1
+            del(aux)
+            return (x)
+
+
+    def recorrer(self): # para eliminar # para recorrer # UNO VA A USAR UN AUXILIAR PARA NO PERDER LA CABEZA (TOPE)
+        if self.vacia():
+            print("La Pila esta vacia No se puede Recorrer")
+        else:
+            aux = self.__tope
+
+            print("Recorriendo la PILA")
+
+            while aux != None:
+                print(f"{aux.retornaDato()}")
+                aux = aux.retornaSig()
+
+
+    def desapilar(self): # para eliminar # para recorrer # UNO VA A USAR UN AUXILIAR PARA NO PERDER LA CABEZA (TOPE)
+        if self.vacia():
+            print("La Pila esta vacia No se puede Desapilar")
+        else:
+            aux = self.__tope
+
+            print("Desapilando la PILA")
+
+            while aux != None:
+                print(f"{self.suprimir()}")
+                aux = aux.retornaSig()
 
 
 
@@ -39,13 +93,10 @@ class tablahash:
     
 
 
-    def __init__(self,dim):
-        self.__dimension = dim
-        self.__tabla = np.empty(dim,dtype=objetoT)
+    def __init__(self,dim):   # para tener listas de 3 NODOS en cada puntero del arreglo se divide el arreglo en 3 para tener un APROXIMADO
 
-        for i in range(self.__dimension):
-            obj = objetoT()
-            self.__tabla[i] = obj
+        self.__dimension = dim // 3
+        self.__tabla = np.empty(self.__dimension,dtype=None)
 
 
     def hashing(self,k,metodo):
@@ -75,6 +126,23 @@ class tablahash:
 
         else:
             return (round(k % self.__dimension))
+
+
+
+
+    def insertar(self,x):
+
+        h = self.hashing(x,'division')
+
+        if 
+
+
+        
+            
+
+
+
+
 
 
 
