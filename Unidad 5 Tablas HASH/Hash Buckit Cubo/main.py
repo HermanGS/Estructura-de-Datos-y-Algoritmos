49bfc55d650ae96f0de03ef514b9396531f2e0f5
Herman Gabriel Soria <103910909+HermanGS@users.noreply.github.com>
2024-10-15 20:49:59 -0300

    Add files via upload

---
+++ Unidad 5 Tablas HASH/Hash Buckit Cubo/main.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+import random
+
+
+class buckit:
+    __arreglo : np.ndarray
+    __ocupados : int
+
+    def __init__(self):
+        pass
+
+class tablahash:
+    __tabla : np.ndarray
+    __dimension : int
+
+
+
+
+
+if __name__ == '__main__':
+    pass
